95721be8c20c9e6ba47cd9f8be91890fbb4b4708
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2023-09-21 07:43:59 +0700

    Initial commit

---
+++ lib/widget/displayText.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget diplayText(){
+  return Card(
+    child: Container(
+      child: Column(
+        children: [
+          Text("data")
+        ],
+      ),
+    ),
+  );
+}
