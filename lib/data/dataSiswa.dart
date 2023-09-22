f389ce5bd67e84058ab92996b4a755dfa9cf78ab
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2023-09-22 16:46:28 +0700

    Home-Page

---
+++ lib/data/dataSiswa.dart
@@ -0,0 +1,21 @@
+class dataSiswa{
+  var siswa = {
+    {
+      "nama" : "Alyaa Rana Raya",
+      "jobdesk" : "",
+      "absen" : 4
+    }, {
+      "nama" : "Inas Tsabitah Dien",
+      "jobdesk" : "Programmer",
+      "absen" : 18
+    }, {
+      "nama" : "Ratih Shinta Puspitasari",
+      "jobdesk" : "",
+      "absen" : 29
+    }, {
+      "nama" : "Rizkia Khosy Amalia W",
+      "jobdesk" : "",
+      "absen" : 30
+    }
+  };
+}
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -6,13 +6,129 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sulawesi Selatan"),
-      ),
-      body: SingleChildScrollView(
+      body: Container(
         child: Stack(
           children: [
-            Image.asset("assets/image/home_page.jpg")
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .89,
+              decoration: BoxDecoration(
+                // color: Colors.black45,
+                image: DecorationImage(
+                  image: AssetImage("lib/assets/image/home_page.jpg"),
+                    fit: BoxFit.cover
+                ),
+              ),
+              child: Container(
+                color: Colors.black.withOpacity(0.7), // Adjust opacity as needed
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 67,
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 17, bottom: 13, left: 37),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Text("Sulawesi Selatan",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 21
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 17, bottom: 13, left: MediaQuery.of(context).size.width * .54),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Text("Sosial Budaya",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 21
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 17, bottom: 13, left: 29),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Text("Ekonomi",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 21
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 17, bottom: 13, left: 29),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Text("Wisata",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 21
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 17, bottom: 13, left: 29),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Text("Kuliner",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 21
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .82),
+                      child: Column(
+                        children: [
+                            Text("PROVINSI SULAWESI SELATAN",
+                              style: TextStyle(
+                                fontSize: 34
+                              ),
+                            ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
