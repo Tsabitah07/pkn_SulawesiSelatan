e4dc3aacb944409dd6dfdd37262a96dd127236b6
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2023-10-13 11:20:59 +0700

    inas-home page

--- lib/data/dataSiswa.dart
+++ lib/data/dataSiswa.dart
@@ -3,18 +3,22 @@ class dataSiswa{
     {
       "nama" : "Alyaa Rana Raya",
       "jobdesk" : "",
+      "image" : "lib/assets/image/profile/alyaa.jpg",
       "absen" : 4
     }, {
       "nama" : "Inas Tsabitah Dien",
       "jobdesk" : "Programmer",
+      "image" : "lib/assets/image/profile/inas.jpeg",
       "absen" : 18
     }, {
       "nama" : "Ratih Shinta Puspitasari",
       "jobdesk" : "",
+      "image" : "lib/assets/image/profile/shinta.jpg",
       "absen" : 29
     }, {
       "nama" : "Rizkia Khosy Amalia W",
       "jobdesk" : "",
+      "image" : "lib/assets/image/profile/kia.jpg",
       "absen" : 30
     }
   };
---
+++ lib/view/desktop.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+
+class DesktopHomePage extends StatelessWidget {
+  const DesktopHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * .88,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  "lib/assets/image/desktop_home_page.jpg"),
+                              fit: BoxFit.cover),
+                        ),
+                        child: Container(
+                          color: Colors.black
+                              .withOpacity(0.7), // Adjust opacity as needed
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.20,
+                          left: MediaQuery.of(context).size.width * 0.13,
+                          right: MediaQuery.of(context).size.width * 0.13,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Image(
+                                  image: AssetImage(
+                                      "lib/assets/image/ibu_kota_sulsel.png"),
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(7),
+                                  child: Text(
+                                    "Makassar, Ibu kota Sulawesi Selatan",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Container(
+                              constraints: BoxConstraints(
+                                  maxWidth:
+                                      MediaQuery.of(context).size.width * 0.2),
+                              child: Text(
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                                textAlign: TextAlign.justify,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      margin: EdgeInsets.all(27),
+                      child: Column(children: [
+                        Text(
+                          "PEMIMPIN SULAWESI SELATAN",
+                          style: TextStyle(fontSize: 29),
+                          textAlign: TextAlign.center,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 27, right: 27, top: 23),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Image.asset(
+                                    "lib/assets/image/gubernur_sulsel.jpg",
+                                    width: MediaQuery.of(context).size.width *
+                                        0.19,
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 23),
+                                    child: Text("Gubernur Sulawesi Selatan"),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Image.asset(
+                                    "lib/assets/image/wakil_gubernur_sulsel.jpg",
+                                    width: MediaQuery.of(context).size.width *
+                                        0.19,
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 23),
+                                    child:
+                                        Text("Wakil Gubernur Sulawesi Selatan"),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ])),
+                  Container(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * .1,
+                          bottom: MediaQuery.of(context).size.height * .1),
+                      child: Column(children: [
+                        Container(
+                          margin: EdgeInsets.all(23),
+                          child: Text(
+                            "HISTORIGRAFI",
+                            style: TextStyle(fontSize: 29),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * .67,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        "lib/assets/image/sosial/histori_background.jpg"),
+                                    fit: BoxFit.fill)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * .4,
+                                  width: MediaQuery.of(context).size.width * .47,
+                                  color: Colors.white,
+                                  padding: EdgeInsets.all(23),
+                                  child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("data"),
+                                        ElevatedButton(
+                                            onPressed: () {},
+                                            child: Text("See detail"))
+                                      ]),
+                                ),
+                              ],
+                            ))
+                      ])),
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                  child: Text(
+                                "ASPEK EKONOMI",
+                                style: TextStyle(fontSize: 29),
+                              )),
+                              Container(
+                                  constraints: BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width *
+                                              0.7),
+                                  child: Text(
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                    textAlign: TextAlign.center,
+                                  )),
+                              Container(
+                                padding: EdgeInsets.only(top: 17, bottom: 11),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Column(children: [
+                                      Image.asset("name"),
+                                      Row(children: [
+                                        Image.asset("name"),
+                                        Image.asset("name")
+                                      ])
+                                    ]),
+                                    Image.asset("name"),
+                                    Column(children: [
+                                      Image.asset("name"),
+                                      Image.asset("name")
+                                    ])
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.all(13),
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius: BorderRadius.circular(21),
+                                ),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("See Details"),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                      child: Column(
+                          children: [
+                            Text("SOSIAL BUDAYA"),
+                          ]
+                      )
+                  ),
+                  Container(
+                      child: Column(
+                          children: []
+                      )
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                padding:
+                    EdgeInsets.only(top: 9, bottom: 9, left: 29, right: 29),
+                height: 67,
+                color: Colors.black54.withOpacity(.2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Sulawesi Selatan",
+                          style: TextStyle(color: Colors.white, fontSize: 21),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 9, right: 9),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Sosial Budaya",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 21),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 9, right: 9),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Ekonomi",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 21),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 9, right: 9),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Wisata",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 21),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 9, right: 9),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Kuliner",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 21),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
