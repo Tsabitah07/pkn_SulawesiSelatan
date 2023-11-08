906589ea5a89843b05052d7410781eda89c8e085
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2023-11-08 14:12:14 +0700

    ulang

--- lib/view/phone.dart
+++ lib/view/phone.dart
@@ -7,6 +7,9 @@ class PhoneHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Stack(
           children: [
@@ -58,10 +61,47 @@ class PhoneHomePage extends StatelessWidget {
                       ],
                     ),
                   ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    height: screenHeight * .7,
+                    width: screenWidth,
+                      child: Column(
+                        children: [
+                          Text("Histori"),
+                          Container(
+                            width: screenWidth * .9,
+                            height: screenHeight * .5,
+                            padding: EdgeInsets.all(10),
+                              margin: EdgeInsets.symmetric(vertical: 15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: AssetImage("lib/assets/image/sosial/histori_background.jpg"),
+                                      fit: BoxFit.cover
+                                  )
+                              ),
+                              child: Container(
+                                margin: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15)
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text("data"),
+                                    Text("data")
+                                  ],
+                                ),
+                              )
+                          ),
+                        ],
+                      ),
+                  )
                 ],
               ),
             ),
-            AppBarPhoneWidget(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height, Colors.white),
+            AppBarPhoneWidget(screenWidth, screenHeight, Colors.white),
             // MenuWidget(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height, Colors.white)
           ]
       ),
